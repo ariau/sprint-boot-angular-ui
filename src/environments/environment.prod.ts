a084111ffbd8677530ff43a1968a635065c17f15
ariau <alexisdeclairebois@gmail.com>
2025-03-13 21:11:56 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://spring-boot-angular-app-hweveuhdbrghb2a9.eastus2-01.azurewebsites.net/'
+  baseUrl: 'https://spring-boot-angular-app2-fzg9cfd8hqagbgge.eastus2-01.azurewebsites.net/'
 };
